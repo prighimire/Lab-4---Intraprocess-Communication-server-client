6125782b1c05bb80735dacf6220c824bd8e3cb58
prighimire <131629703+prighimire@users.noreply.github.com>
2024-12-09 22:40:33 -0500

    initial commit

--- serv.c
+++ serv.c
@@ -1,117 +1,119 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <netinet/in.h> //structure for storing address information 
+#include <stdio.h> 
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include "list.h" // Include your linked list header file
+#include <stdlib.h> 
+#include <sys/socket.h> //for socket APIs 
+#include <sys/types.h> 
+#include "list.h"
 
-#define PORT 8080
-#define ACK "ACK: "
-
+#define PORT 9001
+#define ACK "ACK"
+  
 int main(int argc, char const* argv[]) 
 { 
-    int n, val, idx;
+  
+	  int n, val, idx;
+    // create server socket similar to what was done in 
+    // client program 
     int servSockD = socket(AF_INET, SOCK_STREAM, 0); 
-    if (servSockD < 0) {
-        perror("Error creating socket");
-        exit(1);
-    }
-    printf("Socket created successfully\n");
-
+  
+    // string store data to recv/send to/from client 
     char buf[1024];
-    char sbuf[1024];
+		char sbuf[1024];
     char* token;
 
+    // define server address 
     struct sockaddr_in servAddr; 
-    list_t *mylist;
+  
+	  // list
+		list_t *mylist;
+
 
     servAddr.sin_family = AF_INET; 
     servAddr.sin_port = htons(PORT); 
     servAddr.sin_addr.s_addr = INADDR_ANY; 
-
-    if (bind(servSockD, (struct sockaddr*)&servAddr, sizeof(servAddr)) < 0) {
-        perror("Error binding");
-        exit(1);
-    }
-    printf("Socket bound successfully\n");
-
-    if (listen(servSockD, 1) < 0) {
-        perror("Error in listen");
-        exit(1);
-    }
-    printf("Listening for connections...\n");
-
+  
+    // bind socket to the specified IP and port 
+    bind(servSockD, (struct sockaddr*)&servAddr, 
+         sizeof(servAddr)); 
+  
+    // listen for connections 
+    listen(servSockD, 1); 
+  
+    // integer to hold client socket. 
     int clientSocket = accept(servSockD, NULL, NULL); 
-    if (clientSocket < 0) {
-        perror("Error accepting connection");
-        exit(1);
-    }
-    printf("Client connected!\n");
 
-    mylist = list_alloc();  // Create the list
+    mylist = list_alloc();  // create the list
 
-    while (1) {
+    while(1){
+        // recvs messages from client socket 
         n = recv(clientSocket, buf, sizeof(buf), 0);
-        buf[n] = '\0';
+				buf[n] = '\0';
 
-        if (n > 0) {
+        if(n > 0){
             token = strtok(buf, " ");
     
-            if (strcmp(token, "exit") == 0) {
-                list_free(mylist);  // Free the linked list
-                exit(1);  // Exit server
-            }
-            else if (strcmp(token, "get_length") == 0) {
-                val = list_length(mylist);  // Get the length
-                sprintf(sbuf, "Length = %d", val);
-            }
-            else if (strcmp(token, "add_back") == 0) {
-                token = strtok(NULL, " ");  // Get value
-                val = atoi(token);  
-                list_add_to_back(mylist, val);  // Add to back
+						if(strcmp(token,"exit") == 0){
+							  list_free(mylist);
+							  exit(1);
+						}
+						else if(strcmp(token,"get_length") == 0){
+								// Make a Call
+								val = list_length(mylist);
+								sprintf(sbuf,"%s%d", "Length = ", val);
+						}
+						else if(strcmp(token,"add_front") == 0){
+							  token = strtok(NULL, " ");  // get next token (value)
+								val = atoi(token);
+								// Make a Call
+								sprintf(sbuf,"%s%d", ACK, val);
+								list_add_to_front(mylist,val);
+						}
+            else if(strcmp(token, "add_back") == 0) {
+                token = strtok(NULL, " ");
+                val = atoi(token);
+                list_add_to_back(mylist, val);
                 sprintf(sbuf, "%s%d", ACK, val);
             }
-            else if (strcmp(token, "add_front") == 0) {
+            else if(strcmp(token, "add_position") == 0) {
                 token = strtok(NULL, " ");
-                val = atoi(token);  
-                list_add_to_front(mylist, val);  // Add to front
+                idx = atoi(token);
+                token = strtok(NULL, " ");
+                val = atoi(token);
+                list_add_at_index(mylist, idx, val);
                 sprintf(sbuf, "%s%d", ACK, val);
             }
-            else if (strcmp(token, "add_position") == 0) {
+            else if(strcmp(token, "remove_back") == 0) {
+                val = list_remove_from_back(mylist);
+                sprintf(sbuf, "Removed %d", val);
+            }
+            else if(strcmp(token, "remove_front") == 0) {
+                val = list_remove_from_front(mylist);
+                sprintf(sbuf, "Removed %d", val);
+            }
+						else if(strcmp(token,"remove_position") == 0){
+							  token = strtok(NULL, " ");
+								idx = atoi(token);
+								// Make a Call
+								val = list_remove_at_index(mylist,idx);
+								sprintf(sbuf,"%s%d", ACK, val);
+						}
+            else if(strcmp(token, "get") == 0) {
                 token = strtok(NULL, " ");
-                idx = atoi(token);  // Get index
-                token = strtok(NULL, " ");
-                val = atoi(token);  // Get value
-                list_add_at_index(mylist, idx, val);  // Add at index
-                sprintf(sbuf, "%s%d", ACK, val);
+                idx = atoi(token);
+                val = list_get_elem_at(mylist, idx);
+                sprintf(sbuf, "Value at index %d is %d", idx, val);
             }
-            else if (strcmp(token, "remove_back") == 0) {
-                val = list_remove_from_back(mylist);  // Remove from back
-                sprintf(sbuf, "Removed from back: %d", val);
-            }
-            else if (strcmp(token, "remove_front") == 0) {
-                val = list_remove_from_front(mylist);  // Remove from front
-                sprintf(sbuf, "Removed from front: %d", val);
-            }
-            else if (strcmp(token, "remove_position") == 0) {
-                token = strtok(NULL, " ");
-                idx = atoi(token);  
-                val = list_remove_at_index(mylist, idx);  // Remove at index
-                sprintf(sbuf, "Removed at index %d: %d", idx, val);
-            }
-            else if (strcmp(token, "get") == 0) {
-                token = strtok(NULL, " ");
-                idx = atoi(token);  
-                val = list_get_elem_at(mylist, idx);  // Get element at index
-                sprintf(sbuf, "Element at index %d: %d", idx, val);
-            }
-            else if (strcmp(token, "print") == 0) {
-                sprintf(sbuf, "%s", listToString(mylist));  // Print list
-            }
+						else if(strcmp(token,"print") == 0){
+							  sprintf(sbuf,"%s", listToString(mylist));
+						}
+						// ADD THE OTHER OPERATIONS
 
-            send(clientSocket, sbuf, sizeof(sbuf), 0);  // Send response
+
+                // send's messages to client socket 
+            send(clientSocket, sbuf, sizeof(sbuf), 0);
         }
-        memset(buf, '\0', sizeof(buf));
+				memset(buf, '\0', 1024);
     }
   
     return 0; 
